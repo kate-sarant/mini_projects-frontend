1f12b9c4d781de95fa3f296752084f7a6b4366e9
kate-sarant <72164559+kate-sarant@users.noreply.github.com>
2022-02-18 16:09:38 +0000

    Add files via upload
     Fully responsive  news - webpage using API from https://newsapi.org/

---
+++ php_project_news/header_script.js
@@ -0,0 +1,37 @@
+//making a let with the  api key link -->  http://api.openweathermap.org/data/2.5/weather?q=Athens&units=metric&appid=45d578be6b42425baf50dd4d11bef893
+// which contains fetchWeather function
+// And taking the data that we want from the weather api key(temp,description,max-temp etc)
+let weather = {
+  apiKey:"45d578be6b42425baf50dd4d11bef893",
+  fetchWeather:function(city){
+    fetch(
+        "http://api.openweathermap.org/data/2.5/weather?q="
+        + city
+        +"&units=metric&appid="
+        + this.apiKey
+    ).then((response) => response.json()).then((data) => this.displayWeather(data));
+
+  },
+  //display api -data
+  displayWeather: function(data){
+    const { name } = data;
+    const { icon, description} = data.weather[0];
+    const { temp ,humidity,temp_max,temp_min,feels_like} = data.main;
+    const {speed} = data.wind;
+
+
+    console.log(name, icon, description, temp ,humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src ="http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+    document.querySelector(".temp").innerText= Math.round(temp) + "℃" ;
+    document.querySelector(".weather").classList.remove("loading");
+
+  },
+};
+
+weather.fetchWeather("Athens");
+
+
+//What is response in JS?
+//constructor. Represents a response from a web request initiated by fetch().
+//fetch(), Request and Response are a new, low level replacement for XMLHttpRequest.
